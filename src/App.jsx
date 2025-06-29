d38d085d222e03fa126b9de7b4b49953616addfd
GVictory1351989 <gvictorygamesin@gmail.com>
2025-06-29 16:02:40 +0530

    add: vercel.json and deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import ReportFooter from './components/ReportFooter';
 
 function App() {
     return (
-        <div className="page-wrapper">
-            <div className="page-content">
+        <div className="min-h-screen flex flex-col bg-white text-black">
+            {/* Content Section */}
+            <div className="flex-grow max-w-3xl mx-auto p-4 shadow-md w-full">
                 <Header />
                 <PatientInfo />
                 <BiochemistryReport />
             </div>
+
+            {/* Footer always sticks to bottom */}
             <ReportFooter />
         </div>
     );
