fd226595a0e1a59bcbec729925241629bd094224
GVictory1351989 <gvictorygamesin@gmail.com>
2025-06-30 09:39:11 +0530

    changes

--- src/components/ReportFooter.jsx
+++ src/components/ReportFooter.jsx
@@ -3,17 +3,11 @@
 function ReportFooter() {
     return (
         <div className="report-footer">
-            {/* Bottom-right image + text */}
             <div className="footer-signature-box">
-                <img
-                    src="/images/signdata.png"
-                    alt="Signature"
-                    className="footer-image"
-                />
+                <img src="/images/signdata.png" alt="Signature" className="footer-image" />
                 <div className="footer-name">ASHISH KUMAR(DMLT)</div>
-                <h2></h2>
             </div>
-            {/* Full-width footer line with disclaimer */}
+
             <div className="footer-line">
                 <p className="footer-disclaimer">
                     This report is not valid for medico legal purpose.
